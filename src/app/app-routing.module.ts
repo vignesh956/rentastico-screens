8da752e60742d9b7dbf3947e9a4de0d3c2a3f9aa
bhargavi <tadiboinabhargavi14@gmail.com>
2024-06-19 13:22:06 +0530

    empty line removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,13 +14,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path:''
-  }
-
- 
-  
+  } 
 ];
 
 @NgModule({
