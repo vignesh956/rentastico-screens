165844a5cf2b9d64086ea6a6ccd6244a21eba028
hrushikesh <rhrushi1432@gmail.com>
2024-06-21 09:39:30 +0530

    fullscreen module created

---
+++ src/app/fullscreen/fullscreen.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FullscreenRoutingModule } from './fullscreen-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FullscreenRoutingModule
+  ]
+})
+export class FullscreenModule { }
